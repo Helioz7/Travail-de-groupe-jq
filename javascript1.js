68fa7733f2c6aeaa7eb58e31d4c18d1d67d7360a
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-06-02 19:17:36 +0200

    modif

--- javascript1.js
+++ javascript1.js
@@ -1,11 +1,13 @@
+
+// cache le paragraphe
 $(document).ready(function() {
     $('#show').click(function() {
-        $('#cacher').toggle();
+        $('#cacher').show();
     });
 });
 
 
-
+// Affichage du paragraphe
 $(document).ready(function() {
     $("#montre").click(function() {
         $("#visible").toggle();
@@ -14,5 +16,11 @@ $(document).ready(function() {
     $("#reset").click(function() {
         $("#cacher").hide();  
         $("#visible").show(); 
+    });
+});
+ 
+$(document).ready(function() {
+    $('#paragraph').click(function() {
+        $('#toggle').toggle();
     });
 });
