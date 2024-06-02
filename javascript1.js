980012bfa5ca1c9e9c6372d4d7f4f64adaebb058
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-06-02 19:51:50 +0200

    ajout

--- javascript1.js
+++ javascript1.js
@@ -23,4 +23,28 @@ $(document).ready(function() {
     $('#paragraph').click(function() {
         $('#toggle').toggle();
     });
+}); 
+
+//4
+$(document).ready(function() {
+    $("#hide-image").click(function() {
+        setTimeout(function() {
+            $("#image4").hide();
+        }, 3000); 
+    });
+
+    $("#reset-image").click(function() {
+        $("#image4").show(); 
+    });
+});
+
+//5
+$(document).ready(function() {
+    $("#toggle-image").click(function() {
+        $("#image5").fadeToggle(2000); 
+    });
+
+    $("#reset-toggle").click(function() {
+        $("#image5").fadeIn(0); 
+    });
 });
